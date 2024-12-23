b742e7ea02315adec762cf148caae39f990ad636
Willem van der Schyff <willemvds@gmail.com>
2024-12-23 11:22:23 +0200

    feat: Add SDL3 and open an empty window.

--- build.zig
+++ build.zig
@@ -36,6 +36,10 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.linkLibC();
+    exe.linkSystemLibrary("SDL3");
+    exe.linkSystemLibrary("SDL3_image");
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,63 @@
 const std = @import("std");
+const sdl3 = @cImport({
+    @cInclude("SDL3/SDL.h");
+});
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
     try bw.flush(); // don't forget to flush!
-}
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try stdout.print("Clipboard PI\n", .{});
+
+    if (sdl3.SDL_Init(sdl3.SDL_INIT_VIDEO) == false) {
+        std.debug.print("SDL init err = {s}\n", .{sdl3.SDL_GetError()});
+        return;
+    }
+
+    const window_w: c_int = 1920;
+    const window_h: c_int = 1080;
+    const window_title = "Clipboard PI";
+    const window_flags =
+        sdl3.SDL_WINDOW_BORDERLESS |
+        sdl3.SDL_WINDOW_INPUT_FOCUS |
+        sdl3.SDL_WINDOW_RESIZABLE;
+
+    var window: ?*sdl3.SDL_Window = undefined;
+    var renderer: ?*sdl3.SDL_Renderer = undefined;
+
+    if (!sdl3.SDL_CreateWindowAndRenderer(
+        window_title,
+        window_w,
+        window_h,
+        window_flags,
+        &window,
+        &renderer,
+    )) {
+        return error.SDL;
+    }
+
+    var quit = false;
+    while (!quit) {
+        _ = sdl3.SDL_RenderClear(renderer);
+        _ = sdl3.SDL_RenderPresent(renderer);
+
+        var e: sdl3.SDL_Event = undefined;
+        while (sdl3.SDL_PollEvent(&e)) {
+            switch (e.type) {
+                sdl3.SDL_EVENT_QUIT => {
+                    quit = true;
+                },
+                sdl3.SDL_EVENT_KEY_DOWN => {
+                    const kdev: sdl3.SDL_KeyboardEvent = e.key;
+                    if (kdev.key == sdl3.SDLK_ESCAPE) {
+                        quit = true;
+                    }
+                },
+                else => {},
+            }
+        }
+    }
 }
