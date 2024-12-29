37a35646ddc7db6084e4ddc793809ff83df70bc1
Willem van der Schyff <willemvds@gmail.com>
2024-12-29 15:06:43 +0200

    feat: Make our text generation slightly more reusable.

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,18 @@ pub fn handleClipboardEvent(ev: sdl3.SDL_ClipboardEvent) void {
     }
 }
 
+pub fn createTextTexture(
+    renderer: *sdl3.SDL_Renderer,
+    font: *sdl3.TTF_Font,
+    colour: sdl3.SDL_Color,
+    msg: []const u8,
+) !*sdl3.SDL_Texture {
+    const textSurface = sdl3.TTF_RenderText_Blended(font, @ptrCast(msg), msg.len, colour);
+    const textTex = sdl3.SDL_CreateTextureFromSurface(renderer, textSurface);
+    sdl3.SDL_DestroySurface(textSurface);
+    return textTex;
+}
+
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
@@ -34,7 +46,7 @@ pub fn main() !void {
         sdl3.SDL_WINDOW_RESIZABLE;
 
     var window: ?*sdl3.SDL_Window = undefined;
-    var renderer: ?*sdl3.SDL_Renderer = undefined;
+    var maybeRenderer: ?*sdl3.SDL_Renderer = undefined;
 
     if (!sdl3.SDL_CreateWindowAndRenderer(
         window_title,
@@ -42,59 +54,52 @@ pub fn main() !void {
         window_h,
         window_flags,
         &window,
-        &renderer,
+        &maybeRenderer,
     )) {
         return error.SDL;
     }
 
-    if (sdl3.TTF_Init() == false) {
-        std.debug.print("SDL_ttf init err = {s}\n", .{sdl3.SDL_GetError()});
-        return error.SDL;
-    }
+    if (maybeRenderer) |renderer| {
+        if (sdl3.TTF_Init() == false) {
+            std.debug.print("SDL_ttf init err = {s}\n", .{sdl3.SDL_GetError()});
+            return error.SDL;
+        }
 
-    //    const SDL_CLOSE_IO = true;
-    const uiFont = sdl3.TTF_OpenFont("/home/willem/Downloads/SourceCodePro/SauceCodeProNerdFont-Regular.ttf", 24);
-    if (uiFont == null) {
-        std.debug.print("font loading err = {s}\n", .{sdl3.SDL_GetError()});
-        return error.SDL;
-    }
+        //    const SDL_CLOSE_IO = true;
+        const uiFont = sdl3.TTF_OpenFont("/home/willem/Downloads/SourceCodePro/SauceCodeProNerdFont-Regular.ttf", 24) orelse {
+            std.debug.print("font loading err = {s}\n", .{sdl3.SDL_GetError()});
+            return error.SDL;
+        };
 
-    const tempText = sdl3.SDL_CreateSurface(600, 200, sdl3.SDL_PIXELFORMAT_RGBA32);
-    if (tempText == null) {
-        std.debug.print("create surface err = {s}\n", .{sdl3.SDL_GetError()});
-        return error.SDL;
-    }
-
-    const green = sdl3.SDL_Color{ .r = 0, .g = 255, .b = 0, .a = 255 };
-
-    var quit = false;
-    while (!quit) {
-        _ = sdl3.SDL_RenderClear(renderer);
+        const green = sdl3.SDL_Color{ .r = 0, .g = 255, .b = 0, .a = 255 };
         const msg = "Clipboard Private Investigator \u{f408}  ";
-        const textSurface = sdl3.TTF_RenderText_Blended(uiFont, msg, msg.len, green);
-        const textTex = sdl3.SDL_CreateTextureFromSurface(renderer, textSurface);
-        sdl3.SDL_DestroySurface(textSurface);
-        const targetRect = sdl3.SDL_FRect{ .w = 1200.0, .h = 100.0 };
-        _ = sdl3.SDL_RenderTexture(renderer, textTex, null, &targetRect);
-        _ = sdl3.SDL_RenderPresent(renderer);
+        const msgTex = try createTextTexture(renderer, uiFont, green, msg);
 
-        var e: sdl3.SDL_Event = undefined;
-        while (sdl3.SDL_PollEvent(&e)) {
-            switch (e.type) {
-                sdl3.SDL_EVENT_QUIT => {
-                    quit = true;
-                },
-                sdl3.SDL_EVENT_KEY_DOWN => {
-                    const kdev: sdl3.SDL_KeyboardEvent = e.key;
-                    if (kdev.key == sdl3.SDLK_ESCAPE) {
+        var quit = false;
+        while (!quit) {
+            _ = sdl3.SDL_RenderClear(renderer);
+            const targetRect = sdl3.SDL_FRect{ .w = @floatFromInt(msgTex.w), .h = @floatFromInt(msgTex.h) };
+            _ = sdl3.SDL_RenderTexture(renderer, msgTex, null, &targetRect);
+            _ = sdl3.SDL_RenderPresent(renderer);
+
+            var e: sdl3.SDL_Event = undefined;
+            while (sdl3.SDL_PollEvent(&e)) {
+                switch (e.type) {
+                    sdl3.SDL_EVENT_QUIT => {
                         quit = true;
-                    }
-                },
-                sdl3.SDL_EVENT_CLIPBOARD_UPDATE => {
-                    const cbev: sdl3.SDL_ClipboardEvent = e.clipboard;
-                    handleClipboardEvent(cbev);
-                },
-                else => {},
+                    },
+                    sdl3.SDL_EVENT_KEY_DOWN => {
+                        const kdev: sdl3.SDL_KeyboardEvent = e.key;
+                        if (kdev.key == sdl3.SDLK_ESCAPE) {
+                            quit = true;
+                        }
+                    },
+                    sdl3.SDL_EVENT_CLIPBOARD_UPDATE => {
+                        const cbev: sdl3.SDL_ClipboardEvent = e.clipboard;
+                        handleClipboardEvent(cbev);
+                    },
+                    else => {},
+                }
             }
         }
     }
