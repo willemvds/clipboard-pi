9e3d92f80d5b4598a5f1748a682e9314e497d18a
Willem van der Schyff <willemvds@gmail.com>
2025-01-02 08:53:25 +0200

    refactor: Wrap the c-style SDL API inside a zig-style function.

--- src/main.zig
+++ src/main.zig
@@ -43,6 +43,38 @@ pub fn createTextTexture(
     return textTex;
 }
 
+const CreateWindowAndRendererResult = struct {
+    window: *sdl3.SDL_Window,
+    renderer: *sdl3.SDL_Renderer,
+};
+
+pub fn createWindowAndRenderer(window_width: c_int, window_height: c_int, window_title: []const u8, window_flags: u64) !CreateWindowAndRendererResult {
+    var maybeWindow: ?*sdl3.SDL_Window = undefined;
+    var maybeRenderer: ?*sdl3.SDL_Renderer = undefined;
+
+    if (!sdl3.SDL_CreateWindowAndRenderer(
+        @ptrCast(window_title),
+        window_width,
+        window_height,
+        window_flags,
+        &maybeWindow,
+        &maybeRenderer,
+    )) {
+        return error.SDL;
+    }
+
+    if (maybeWindow) |window| {
+        if (maybeRenderer) |renderer| {
+            return CreateWindowAndRendererResult{
+                .window = window,
+                .renderer = renderer,
+            };
+        }
+    }
+
+    unreachable();
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const ally = gpa.allocator();
@@ -67,76 +99,63 @@ pub fn main() !void {
         sdl3.SDL_WINDOW_INPUT_FOCUS |
         sdl3.SDL_WINDOW_RESIZABLE;
 
-    var window: ?*sdl3.SDL_Window = undefined;
-    var maybeRenderer: ?*sdl3.SDL_Renderer = undefined;
+    const createWindowAndRendererResult = try createWindowAndRenderer(window_w, window_h, window_title, window_flags);
+    const renderer = createWindowAndRendererResult.renderer;
 
-    if (!sdl3.SDL_CreateWindowAndRenderer(
-        window_title,
-        window_w,
-        window_h,
-        window_flags,
-        &window,
-        &maybeRenderer,
-    )) {
+    if (sdl3.TTF_Init() == false) {
+        std.debug.print("SDL_ttf init err = {s}\n", .{sdl3.SDL_GetError()});
         return error.SDL;
     }
 
-    if (maybeRenderer) |renderer| {
-        if (sdl3.TTF_Init() == false) {
-            std.debug.print("SDL_ttf init err = {s}\n", .{sdl3.SDL_GetError()});
-            return error.SDL;
+    //    const SDL_CLOSE_IO = true;
+    const uiFont = sdl3.TTF_OpenFont("/home/willem/Downloads/SourceCodePro/SauceCodeProNerdFont-Regular.ttf", 24) orelse {
+        std.debug.print("font loading err = {s}\n", .{sdl3.SDL_GetError()});
+        return error.SDL;
+    };
+
+    const green = sdl3.SDL_Color{ .r = 0, .g = 255, .b = 0, .a = 255 };
+    const msg = "Clipboard Private Investigator \u{f408}  ";
+    const msgTex = try createTextTexture(renderer, uiFont, green, msg);
+
+    var labelTextures = try std.ArrayList(*sdl3.SDL_Texture).initCapacity(ally, 0);
+
+    var quit = false;
+    while (!quit) {
+        _ = sdl3.SDL_RenderClear(renderer);
+        const targetRect = sdl3.SDL_FRect{ .w = @floatFromInt(msgTex.w), .h = @floatFromInt(msgTex.h) };
+        _ = sdl3.SDL_RenderTexture(renderer, msgTex, null, &targetRect);
+
+        for (labelTextures.items, 0..) |labelTexture, idx| {
+            const y: f32 = @floatFromInt(idx * @as(usize, @intCast(labelTexture.h)));
+            const tr = sdl3.SDL_FRect{
+                .x = 10.0,
+                .y = 100.0 + y,
+                .w = @floatFromInt(labelTexture.w),
+                .h = @floatFromInt(labelTexture.h),
+            };
+            _ = sdl3.SDL_RenderTexture(renderer, labelTexture, null, &tr);
         }
 
-        //    const SDL_CLOSE_IO = true;
-        const uiFont = sdl3.TTF_OpenFont("/home/willem/Downloads/SourceCodePro/SauceCodeProNerdFont-Regular.ttf", 24) orelse {
-            std.debug.print("font loading err = {s}\n", .{sdl3.SDL_GetError()});
-            return error.SDL;
-        };
+        _ = sdl3.SDL_RenderPresent(renderer);
 
-        const green = sdl3.SDL_Color{ .r = 0, .g = 255, .b = 0, .a = 255 };
-        const msg = "Clipboard Private Investigator \u{f408}  ";
-        const msgTex = try createTextTexture(renderer, uiFont, green, msg);
-
-        var labelTextures = try std.ArrayList(*sdl3.SDL_Texture).initCapacity(ally, 0);
-
-        var quit = false;
-        while (!quit) {
-            _ = sdl3.SDL_RenderClear(renderer);
-            const targetRect = sdl3.SDL_FRect{ .w = @floatFromInt(msgTex.w), .h = @floatFromInt(msgTex.h) };
-            _ = sdl3.SDL_RenderTexture(renderer, msgTex, null, &targetRect);
-
-            for (labelTextures.items, 0..) |labelTexture, idx| {
-                const y: f32 = @floatFromInt(idx * @as(usize, @intCast(labelTexture.h)));
-                const tr = sdl3.SDL_FRect{
-                    .x = 10.0,
-                    .y = 100.0 + y,
-                    .w = @floatFromInt(labelTexture.w),
-                    .h = @floatFromInt(labelTexture.h),
-                };
-                _ = sdl3.SDL_RenderTexture(renderer, labelTexture, null, &tr);
-            }
-
-            _ = sdl3.SDL_RenderPresent(renderer);
-
-            var e: sdl3.SDL_Event = undefined;
-            while (sdl3.SDL_PollEvent(&e)) {
-                switch (e.type) {
-                    sdl3.SDL_EVENT_QUIT => {
+        var e: sdl3.SDL_Event = undefined;
+        while (sdl3.SDL_PollEvent(&e)) {
+            switch (e.type) {
+                sdl3.SDL_EVENT_QUIT => {
+                    quit = true;
+                },
+                sdl3.SDL_EVENT_KEY_DOWN => {
+                    const kdev: sdl3.SDL_KeyboardEvent = e.key;
+                    if (kdev.key == sdl3.SDLK_ESCAPE) {
                         quit = true;
-                    },
-                    sdl3.SDL_EVENT_KEY_DOWN => {
-                        const kdev: sdl3.SDL_KeyboardEvent = e.key;
-                        if (kdev.key == sdl3.SDLK_ESCAPE) {
-                            quit = true;
-                        }
-                    },
-                    sdl3.SDL_EVENT_CLIPBOARD_UPDATE => {
-                        const cbev: sdl3.SDL_ClipboardEvent = e.clipboard;
-                        const cbResult = try handleClipboardEvent(ally, cbev, renderer, uiFont);
-                        labelTextures = cbResult.textures;
-                    },
-                    else => {},
-                }
+                    }
+                },
+                sdl3.SDL_EVENT_CLIPBOARD_UPDATE => {
+                    const cbev: sdl3.SDL_ClipboardEvent = e.clipboard;
+                    const cbResult = try handleClipboardEvent(ally, cbev, renderer, uiFont);
+                    labelTextures = cbResult.textures;
+                },
+                else => {},
             }
         }
     }
